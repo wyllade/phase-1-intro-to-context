ab415df9a18efaf85b6cf8274cef9f178a45ddd2
Emman Oduor <wyllademanu003@gmail.com>
2025-04-01 11:27:52 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+
+function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(employee, dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    employee.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour) });
+    return employee;
+}
+
+function createTimeOutEvent(employee, dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    employee.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour) });
+    return employee;
+}
+
+function hoursWorkedOnDate(employee, date) {
+    let timeIn = employee.timeInEvents.find(e => e.date === date);
+    let timeOut = employee.timeOutEvents.find(e => e.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(employee, date) {
+    return hoursWorkedOnDate(employee, date) * employee.payPerHour;
+}
+
+function allWagesFor(employee) {
+    return employee.timeInEvents.reduce((total, event) => total + wagesEarnedOnDate(employee, event.date), 0);
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => total + allWagesFor(employee), 0);
+}
